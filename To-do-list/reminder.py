a10c3bf053c72dcd07c366129ac579a230daeb88
SHIVANGI <shivangibitsp@gmail.com>
2018-06-14 23:10:22 +0000

    script for playing the reminder audio
    Please find bugs in it and help rectify
    Please check on the following terms first:
    1.) importing of the list file
    2.) format for attribute of playsound that should be audio path as set in create_list
    3.)line 20 and 21

---
+++ To-do-list/reminder.py
@@ -0,0 +1,21 @@
+#pip install playsound
+
+import create_list
+import os
+import subprocess
+from datetime import date
+from playsound import playsound
+
+
+def checkTasks():
+  list2 = [item[1] for task in finalList]
+  today = date.today()
+  flag = 0
+  for date in list2:
+    if date == today:
+      flag = 1
+      playsound(default_audio)
+  if flag == 0:
+    os.system('notify-send "No task for today" ')
+if __name__ == '__main__':
+  checkTasks()
