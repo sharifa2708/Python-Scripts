078ef6f15e7d5a50d43162979cfd6d8d9afde616
SHIVANGI <shivangibitsp@gmail.com>
2018-06-15 15:43:26 +0000

    Create reminder_script.py

---
+++ To-do-list/reminder_script.py
@@ -0,0 +1,29 @@
+#pip install python-vlc
+from datetime import date
+import vlc
+import os
+import time
+
+
+
+
+
+def checkTasks():
+	list2 = [item[1] for task in finalList]
+	today = date.today()
+	flag = 0
+
+#if the due date matches today's date play the audio if not notify
+#No task for that day
+	for date in list2:
+		if date == today:
+			flag =1
+			p = vlc.MediaPlayer("default_audio")
+		        p.play()
+	if flag == 0:
+		os.system(' notify-send "No task for today" ')
+
+if __name__ == '__main__' :
+	checkTasks()
+	
+	
